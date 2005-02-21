46f714994868bb7f4daac8057be1cba695e69d92
Rafael Laboissiere <rafael@debian.org>
2005-02-21 20:51:22 +0000

    Removed obsolete files
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-octave/trunk/packages/octave2.1@81 182a0fab-b9ee-0310-b8e4-859efd042c02

--- debian/cvs_it.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cvs -d :pserver:anoncvs@www.che.wisc.edu:/cvs login 
-cvs -d :pserver:anoncvs@www.che.wisc.edu:/cvs -z 9 checkout octave 
-cvs -d :pserver:anoncvs@www.che.wisc.edu:/cvs logout
