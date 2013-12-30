a2c342e694c9970f8ee797cf142c0814c85c4c7c
Mike Miller <mtmiller@debian.org>
2013-12-30 13:09:42 -0500

    Use mtmiller@debian.org for my copyright.
    Git-Dch: Ignore

--- debian/debian_missing_handler.m
+++ debian/debian_missing_handler.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2013 Mike Miller  <mtmiller@ieee.org>
+## Copyright (C) 2013 Mike Miller <mtmiller@debian.org>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
